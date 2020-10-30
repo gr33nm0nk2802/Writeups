32a47928ed0d21e63b0778b96d820b849748dc85
annonymousmaddy <36469557+annonymousmaddy@users.noreply.github.com>
2020-10-30 19:52:42 +0530

    Update pop.sh

--- HactivityCon2020/pop.sh
+++ HactivityCon2020/pop.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# Hacktivitycon recursive zip writeup
 cd test/
 
+# specify the file
 f='pop.zip'
 
+# Infinite loop
 while [ 1 ] 
 do
-
+	# check the filetype
 	file $f | grep 'ASCII' > /dev/null
 	if [ "$?" -eq "1" ]; then
+		# Extract files reverting the errors
 		7z x $f > /dev/null
 		rm $f
 		f=$(ls)
